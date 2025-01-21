3b459f0542af27a2063c4f91fa4765237e8b9b06
Suyash Dabral <suyashdabral05@gmail.com>
2025-01-21 23:24:03 +0530

    added files

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,62 @@
+import { BrowserRouter } from "react-router-dom"
+import { Container } from "./styles"
+import ScrollAnimation from "react-animate-on-scroll"
+import Illustration from "../../assets/illustration.svg"
+import { NavHashLink } from "react-router-hash-link"
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import Hello from '../../assets/Hello.gif'
+import telegram from '../../assets/telegram.svg'
+export function Hero() {
+  return (
+    <Container id="home">
+      <div className="hero-text">
+        <ScrollAnimation animateIn="fadeInUp">
+          <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I am</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
+          <h1>Suyash Dabral</h1>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
+          <h3>Software Engineer</h3>
+        </ScrollAnimation>
+       <br></br>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
+          <BrowserRouter>
+            <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
+          </BrowserRouter>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
+      <div className="social-media"><a
+        href="https://www.linkedin.com/in/suyash-d-664011195/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={linkedin} alt="Linkedin" />
+      </a>
+        <a
+          href="https://github.com/suyash-dabral"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        
+        <a
+          href="https://t.me/suyashd05"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a></div>
+        </ScrollAnimation>
+      </div>
+      {/* <div className="hero-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
+          <img src={Illustration} alt="Ilustração" />
+        </ScrollAnimation>
+      </div> */}
+    </Container>
+  )
+}
